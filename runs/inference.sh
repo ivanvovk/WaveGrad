daa524eb582379096a6488d8ade3988173b45db1
Ivan Vovk <ivanvovk4@yandex.ru>
2020-10-24 01:30:17 +0300

    CLI inference support

---
+++ runs/inference.sh
@@ -0,0 +1,9 @@
+export CUDA_VISIBLE_DEVICES=0
+
+CONFIG_PATH=$1  #configs/default.json
+CHECKPOINT_PATH=$2  #logs/default/checkpoint_57888.pt
+NOISE_SCHEDULE_PATH=$3  #schedules/default/6iters.pt
+MEL_FILELIST_PATH=$4  #tmp/mel_filelist.txt
+VERBOSE=$5   #'yes'
+
+python inference.py -c $CONFIG_PATH -ch $CHECKPOINT_PATH -ns $NOISE_SCHEDULE_PATH -m $MEL_FILELIST_PATH -v $VERBOSE
